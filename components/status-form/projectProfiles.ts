0e7985592837a48b20fbae04a11e8607c9823365
swissarmychainsaw <swissarmychainsaw@gmail.com>
2025-09-08 18:42:59 -0700

    Update projectProfiles.ts

--- components/status-form/projectProfiles.ts
+++ components/status-form/projectProfiles.ts
@@ -151,7 +151,7 @@ export const PROJECT_PROFILES: Record<BannerKey, ProjectProfile> = {
     },
     defaults: {
       programTitle: "Azure Odyssey Program Status",
-      programSummary: ""Making Azure a First-Class Citizen at LinkedIn" drives developer productivity, strengthens security, and streamlines operations and scalability. The program spans multiple tracks—Ownership & Management, Infrastructure & Deployment, Observability & Troubleshooting, Cost & Quota, Advanced Azure Functionalities, and Data & Governance. For more details, see program resources at go/Azure-Odyssey.",
+      programSummary: "Making Azure a First-Class Citizen at LinkedIn drives developer productivity, strengthens security, and streamlines operations and scalability. The program spans multiple tracks—Ownership & Management, Infrastructure & Deployment, Observability & Troubleshooting, Cost & Quota, Advanced Azure Functionalities, and Data & Governance. For more details, see program resources at go/Azure-Odyssey.",
       tpm: "Nick Adams",
       engDri: "Mario Estevez-Cruz",
       bizSponsor: "Niha Mathur",
