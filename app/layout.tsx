50172f36b0e85a51878a62fb1cee92a3edfb192c
swissarmychainsaw <swissarmychainsaw@gmail.com>
2025-09-10 13:56:08 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import { Suspense } from "react"
 import "./globals.css"
 
